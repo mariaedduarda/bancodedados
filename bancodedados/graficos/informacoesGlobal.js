f64b9f25d7c07c042253fdbb7452a324a1ac57a7
mariaedduarda <119062239+mariaedduarda@users.noreply.github.com>
2024-10-21 12:13:15 -0300

    Delete bancodedados directory

--- bancodedados/graficos/informacoesGlobal.js
+++
@@ -1,9 +0,0 @@
-// const url='https://raw.githubusercontent.com/guilhermeonrailis/api/main/dados-globais.json';
-
-//url='20.201.28.151/main/dados/grafico-principal.json';
-//modelo de endereço servidor de dados para o javaScript.
-
-
-async function visualizarInformacoesGlobias()   
-    const res = await fetch(url);
-    const dados = await res.json;
